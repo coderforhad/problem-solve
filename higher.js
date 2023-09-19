19b931563d1f7d32c23cacee556a3275de27a7ff
coderforhadahmed <forhad@fanfare.com.bd>
2023-09-19 14:04:39 +0600

    reduce problem added

--- higher.js
+++ higher.js
@@ -59,12 +59,12 @@
 
 // console.log(outputMin);
 
-const user = [
-    { firstName: "akshay", lastName: "saini", age: 26 },
-    { firstName: "donald", lastName: "trump", age: 75 },
-    { firstName: "elon", lastName: "musk", age: 50 },
-    { firstName: "deepika", lastName: "padukone", age: 26 },
-]
+// const user = [
+//     { firstName: "akshay", lastName: "saini", age: 26 },
+//     { firstName: "donald", lastName: "trump", age: 75 },
+//     { firstName: "elon", lastName: "musk", age: 50 },
+//     { firstName: "deepika", lastName: "padukone", age: 26 },
+// ]
 
 // const output = user.reduce((acc, curr)=>{
 //     if(curr.age <= 30 ){
@@ -73,13 +73,99 @@ const user = [
 //     return acc;
 // }, [])
 
-const output = user.reduce((acc, curr) => {
-    if (acc[curr.age]) {
-        acc[curr.age] = ++acc[curr.age];
+// const output = user.reduce((acc, curr) => {
+//     if (acc[curr.age]) {
+//         acc[curr.age] = ++acc[curr.age];
+//     } else {
+//         acc[curr.age] = 1;
+//     }
+//     return acc;
+// }, {})
+
+// console.log('output', output)
+
+
+// const largest =  arr.reduce((acc, curr) =>curr < acc ? curr : acc)
+
+// console.log('largest', largest)
+
+// const secondLargest = arr.reduce((max, current) => {
+//     if (current > max.max) {
+//       return { max: current, secondMax: max.max };
+//     } else if (current > max.secondMax && current !== max.max) {
+//       return { max: max.max, secondMax: current };
+//     }
+//     return max;
+//   }, { max: -Infinity, secondMax: -Infinity });
+
+//   console.log(secondLargest);
+
+// const secondLargest = arr.reduce((acc, curr) => {
+//     if (curr > acc.largest) {
+//         return { largest: curr, secondLargest: acc.largest }
+//     } else if (curr > acc.largest && curr !== acc.largest) {
+//         return { largest: curr, secondLargest: acc.secondLargest }
+//     }
+//     return acc
+
+// }, { largest: -Infinity, secondLargest: -Infinity })
+
+// console.log(secondLargest)
+
+//descending order
+// console.log(arr.sort().reverse());
+
+// const output = arr.reduce((acc, curr) => {
+//     if (curr > acc.largest) { return { largest: curr, secondLargest: acc.largest } } else if (curr > acc.largest && curr !== acc.largest) {
+//         return { largest: curr, secondLargest: acc.secondLargest }
+//     }
+//     return acc;
+// }, { largest: -Infinity, secondLargest: -Infinity })
+
+// console.log(output)
+
+// function isPrime(num) {
+//     if (num <= 1) return false;
+//     if (num <= 3) return true;
+//     if (num % 2 === 0 || num % 3 === 0) return false;
+//     for (let i = 5; i * i < num; i += 6) {
+//         if (num % i === 0 && num % (i + 2) === 0) {
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+// const primeNumbers = arr.filter(isPrime);
+
+// console.log(primeNumbers);
+
+// const arr = [2, 3, 23, 20, 10, 35];
+
+// const output = arr.filter((x)=> x % 2 ? x : "")
+
+// console.log(output)
+
+// function factorialRecursive(n) {
+//     if (n === 0 || n === 1) {
+//         return 1
+//     } else {
+//         return n * factorialRecursive(n - 1);
+//     }
+// }
+
+// console.log(factorialRecursive(3))
+
+
+const arr = [2, , 23, 20, 10, 35];
+
+const output = arr.reduce((acc, curr) => {
+    if (acc[curr]) {
+        acc[curr] = ++acc[curr];
     } else {
-        acc[curr.age] = 1;
+        acc[curr] = 1;
     }
     return acc;
 }, {})
 
-console.log('output', output)
+console.log(output)
